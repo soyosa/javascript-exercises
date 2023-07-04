98429bf870dc9b372b1b3545408e2f5369e2b7a1
soyosa <110913089+soyosa@users.noreply.github.com>
2023-07-04 20:07:05 +0100

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeats) {
+ if (repeats < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < repeats; i++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
