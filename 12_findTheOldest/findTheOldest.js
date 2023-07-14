16d7feb6bde2a48e18dfc98fdb53848a98190319
soyosa <110913089+soyosa@users.noreply.github.com>
2023-07-14 17:50:11 +0100

    Update findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  let oldestPerson = array[0];
 
+  for (let i = 1; i < array.length; i++) {
+    const newtPerson = array[i];
+    if (getAge(oldestPerson) < getAge(newPerson)) {
+      oldestPerson = newPerson;
+    }
+  }
+
+  return oldestPerson;
+}
+
+function getAge(person) {
+  const currentYear = new Date().getFullYear();
+  const deathYear = person.yearOfDeath || currentYear;
+  const age = deathYear - person.yearOfBirth;
+  return age;
 };
 
 // Do not edit below this line
